d72313e0cd8f52c07c75761e99b1fd3d21b5bea4
jadilovic <adilovic79@yahoo.com>
2021-11-17 00:39:44 +0100

    26 heroku refresh fix

--- backend/server.js
+++ backend/server.js
@@ -40,15 +40,8 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/tasks', authenticateUser, tasksRouter);
 
 // added for heroku
-app.get('*', (req, res) => {
-	res.sendFile(
-		path.join(
-			`${process.env.REACT_APP_SERVER_URL}`,
-			'frontend',
-			'build',
-			'index.html'
-		)
-	);
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 // app.get('/*', function (req, res) {
 // 	console.log('test test test');
