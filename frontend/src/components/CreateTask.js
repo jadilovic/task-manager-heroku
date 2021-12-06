b68cdb41382f14884012bd4681d9bc360dec768b
jadilovic <adilovic79@yahoo.com>
2021-12-06 11:33:16 +0100

    final commit or I hope so

--- frontend/src/components/CreateTask.js
+++ frontend/src/components/CreateTask.js
@@ -29,9 +29,7 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
 	},
 	'& .MuiInputBase-input': {
 		borderRadius: 4,
-		// position: 'relative',
 		border: '1px solid #ced4da',
-		// fontSize: 16,
 		padding: '10px 26px 10px 12px',
 	},
 }));
